4885c3236cca9c41a5cef1cb9074cafa90b5a1a8
Josh <j-kozlowski@live.com>
2015-12-07 06:20:13 -0500

    added whitenoise

--- mapplop/wsgi.py
+++ mapplop/wsgi.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mapplop.settings")
 
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
