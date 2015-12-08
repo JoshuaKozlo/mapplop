73e308fcf29bba39b8bcb73c58df5586cf98e2f6
Josh <j-kozlowski@live.com>
2015-12-08 18:05:32 -0500

    added settings for serving static files with aws s3

--- mapplop/settings.py
+++ mapplop/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = (
     'django.contrib.gis',
     'rest_framework',
     'rest_framework_gis',
+    'storages',
     'venues',
 )
 
@@ -101,6 +102,8 @@ DATABASES = {
     }
 }
 
+
+
 SERIALIZATION_MODULES = {
     'geojson': 'django.contrib.gis.serializers.geojson',
 }
@@ -140,3 +143,20 @@ STATICFILES_DIRS =(
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = 'media'
+
+# Amazon S3 
+
+AWS_STORAGE_BUCKET_NAME = 'mapplopstaticmedia'
+AWS_ACCESS_KEY_ID = 'AKIAJLROC5E62X2OR35Q'
+AWS_SECRET_ACCESS_KEY = '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'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'Cache-Control': 'max-age=94608000',
+    }
