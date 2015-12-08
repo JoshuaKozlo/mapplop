4072756e5ffbe14a72c8565ced46d0901801e98d
Josh <j-kozlowski@live.com>
2015-12-07 23:06:56 -0500

    made changes to settings.py

--- mapplop/wsgi.py
+++ mapplop/wsgi.py
@@ -8,12 +8,14 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-from dj_static import Cling
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mapplop.settings")
 
-application = Cling(get_wsgi_application())
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+try:
+	from dj_static import Cling
+	application = Cling(get_wsgi_application())
+except:
+	pass
 
