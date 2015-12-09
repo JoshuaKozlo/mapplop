924218a58eae8f6ca0a54c3b35049712f997aed5
Josh <j-kozlowski@live.com>
2015-12-09 01:50:08 -0500

    made final production changes

--- static/js/addvenue.js
+++ static/js/addvenue.js
@@ -1,6 +1,6 @@
 $.ajax({
 	method: "GET",
-	url: '/static/json/statelist.json'
+	url: 'https://s3.amazonaws.com/mapplopstaticmedia/static/json/statelist.json'
 }).done(function( data ){
 	for (var i = 0; i < data.length; i++) {
 		$("select").append('<option>' + data[i].name + '</option>');
