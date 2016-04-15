25e8c4821b46ad95a3977455d0a042932716d9c2
Josh <j-kozlowski@live.com>
2016-04-15 18:38:40 -0400

    added django storages

--- mapplop/settings.py
+++ mapplop/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'storages',
     'rest_framework',
     'rest_framework_gis',
     'storages',
